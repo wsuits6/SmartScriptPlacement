ac4b14a997e9e6bf10f0118d8c9a5af2cf413172
wsuits6 <wsuits6@gmail.com>
2026-02-01 06:26:45 +0000

    hehe

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@
 //by wsuits6
 //============================================
 
+// 
 
+//selecting  button
 
+const button = document.getElementById("btn");
+const para  = document.getElementById("paragraph")
+
+//Creating Event listener Funtion
+button.addEventListener(onclick, () => {
+    para.innerText = "Javascript is running"
+})
